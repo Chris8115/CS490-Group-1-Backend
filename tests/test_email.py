0c60e4f6f361600693835475a1bf1dc592eb26d6
Chris8115 <chrisdefeo8115@gmail.com>
2025-04-19 13:02:53 -0400

    Import order adjustment again
    Import order adjustment again

--- tests/test_email.py
+++ tests/test_email.py
@@ -1,8 +1,8 @@
 # tests/test_email.py
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import os
 import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import pytest
 from flask_mail import Message
 from sqlalchemy import text

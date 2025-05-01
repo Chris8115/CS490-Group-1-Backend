85494618612b73aa45db47aae80e191cde17b8e7
Chris8115 <chrisdefeo8115@gmail.com>
2025-05-01 19:06:34 -0400

    test

--- UI_Testing/test_1_signup_patient.py
+++ UI_Testing/test_1_signup_patient.py
@@ -4,8 +4,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import datetime
 
-import time
-
 def test_patient_registration():
     driver = webdriver.Chrome()
     driver.get("http://localhost:3000/sign-up")

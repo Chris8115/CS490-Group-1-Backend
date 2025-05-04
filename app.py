c16e9d4b19ec1e5dd870ea5a7b4b056a08cdc115
ryanrnjit <rjr@njit.edu>
2025-05-04 12:43:43 -0400

    imported threading on the correct file

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ from flask_login import LoginManager, UserMixin, login_user, LoginManager, login
 from datetime import datetime, timedelta, timezone, date
 import json
 import requests
+import threading
 from flask_mail import Mail, Message
 from flask import send_from_directory
 import sys

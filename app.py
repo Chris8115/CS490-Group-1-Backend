e173ea364db00e88d820c6ebb992a5123b5a5ed3
Chris8115 <chrisdefeo8115@gmail.com>
2025-05-04 22:26:39 -0400

    Another domain adjustment

--- app.py
+++ app.py
@@ -2667,7 +2667,7 @@ if __name__ == "__main__":
         print("****You are not in debug mode, add any 1 param to run as localhost****") #test
         PORT = '5000'
         HOST = os.getenv("SECRET_HOST")
-        public_host = app.config.get('PUBLIC_HOST', 'http://localhost')
+        public_host = app.config.get('PUBLIC_HOST', 'localhost')
         port = int(os.getenv("PORT", 5000))
         app.run(host=HOST, port=port) 
     
